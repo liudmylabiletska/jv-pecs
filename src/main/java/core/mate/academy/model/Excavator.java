9b67837f979c05193030138c8219684c8ab78094
Bohdan Chupika <boroda4436@users.noreply.github.com>
2023-11-13 13:06:56 +0200

    remove encoding config from checkstyle plugin

--- src/main/java/core/mate/academy/model/Excavator.java
+++ src/main/java/core/mate/academy/model/Excavator.java
@@ -13,3 +13,5 @@ public class Excavator extends Machine {
         System.out.println("Excavator started to work");
     }
 }
+
+
--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -1,7 +1,38 @@
 package core.mate.academy.service;
 
-/**
- * Your implementation of MachineService.
- */
-public class MachineServiceImpl {
+import core.mate.academy.model.Bulldozer;
+import core.mate.academy.model.Excavator;
+import core.mate.academy.model.Machine;
+import core.mate.academy.model.Truck;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MachineServiceImpl<T extends Machine> implements MachineService<T> {
+
+    @Override
+    public List<T> getAll(Class<T> type) {
+        List<? extends Machine> machines;
+        if (type == Bulldozer.class) {
+            machines = new BulldozerProducer().get();
+        } else if (type == Excavator.class) {
+            machines = new ExcavatorProducer().get();
+        } else if (type == Truck.class) {
+            machines = new TruckProducer().get();
+        } else {
+            return List.of();
+        }
+        return new ArrayList<>((List<T>) machines);
+    }
+
+    @Override
+    public void fill(List<? super T> machines, T value) {
+        for (int i = 0; i < machines.size(); i++) {
+            machines.set(i, value);
+        }
+    }
+
+    @Override
+    public void startWorking(List<? extends T> machines) {
+        machines.forEach(Machine::doWork);
+    }
 }
