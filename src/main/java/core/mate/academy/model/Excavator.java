279e2e53f56d84a07ef37e29552b700a3b0336d6
Biletska Liudmyla <bileckaludmila74@gmail.com>
2025-05-29 15:45:51 +0200

    Fix: Added unique Excavator field and corrected method reference in MachineServiceImpl

--- src/main/java/core/mate/academy/model/Excavator.java
+++ src/main/java/core/mate/academy/model/Excavator.java
@@ -5,13 +5,18 @@ package core.mate.academy.model;
  * Do not remove no-args constructor
  */
 public class Excavator extends Machine {
-    public Excavator() {
+    private int bucketCapacity;
+
+    public int getBucketCapacity() {
+        return bucketCapacity;
+    }
+
+    public void setBucketCapacity(int bucketCapacity) {
+        this.bucketCapacity = bucketCapacity;
     }
 
     @Override
     public void doWork() {
-        System.out.println("Excavator started to work");
+        System.out.println("Excavator is working with bucket capacity: " + bucketCapacity);
     }
 }
-
-
